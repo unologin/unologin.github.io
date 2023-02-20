1cd6e42fef0f3073866f7c419b1a8f6e8483b30f
adrian-kriegel <adrian@unolog.in>
2023-02-20 11:14:41 +0100

    Added next-sdk; Changed directory structure.

--- update-docs.sh
+++ update-docs.sh
@@ -3,11 +3,12 @@
 projects=(\
   "node-api" \
   "web-sdk" \
+  "next-sdk"
 )
 
 for project in ${projects[@]}; do
   svn export \
     --force \
     "https://github.com/unologin/$project.git/trunk/docs" \
-    "docs/$project"
+    "$project"
 done
